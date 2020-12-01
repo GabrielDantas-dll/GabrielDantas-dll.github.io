2cbadd5f386378353cd457075d1a555d9f50c918
GabrielDantas-dll <gabrieldol@yahoo.com.br>
2020-12-01 19:09:23 -0300

    fazendo algumas modificações

--- js/default.js
+++ js/default.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
    $('.list-menu a').on('click', function(){
-        $('.list-menu a').removeClass('selected');
-        $(this).addClass('selected');
-   }); 
+      $('.list-menu a').removeClass('selected');
+      $(this).addClass('selected');
+      let area = $(this).data('area');
+      $('.home').removeClass('show');
+      $('.' + area).addClass('show');
+
+   });
 });
