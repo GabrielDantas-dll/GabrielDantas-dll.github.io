66f510f81c5ac91ed5432fac2e5dbbdd7c7643a9
GabrielDantas-dll <gabrieldol@yahoo.com.br>
2020-12-11 15:18:18 -0300

    Adicionando carousel

--- js/default.js
+++ js/default.js
@@ -5,6 +5,24 @@ $(document).ready(function(){
       let area = $(this).data('area');
       $('.home').removeClass('show');
       $('.' + area).addClass('show');
-
    });
+
+   $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:false,
+      dots: true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:2
+          }
+      }
+  })
+
 });
